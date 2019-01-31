ac1a938c363f92b8964102664736468ecd3e5872
VictorAung <VictorAung2021@u.northwestern.edu>
2019-01-31 02:27:41 -0600

    started on csv export

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,73 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  AsyncStorage,
+  TextInput,
+  Button,
+  } from 'react-native';
+
+import {
+    FileSystem
+  } from 'expo';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
-    title: 'app.json',
+    header: null,
+  };
+
+  writeToFile = async () => {
+    const fileContents = 'This is a my content.';
+    try {
+      await FileSystem.writeAsStringAsync(FileSystem.documentDirectory + 'myDirectory/myFile.txt', fileContents);
+    }
+    catch(error) {
+      alert(error);
+      fileContents = await FileSystem.readAsStringAsync(FileSystem.documentDirectory + 'myDirectory/myFile.txt');
+      alert("read from file:" + fileContents);
+    }
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
-    return <ExpoConfigView />;
+    return(
+    <View style={styles.container}>
+      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <View style={styles.logoContainer}>
+          <Image
+          style={{width: 300, height: 150}}
+          source={require ('../assets/images/ourly.png')}
+          />
+        </View>
+        <Button
+          onPress={this.writeToFile}
+          title="Export to .CSV"
+          color="#841584"
+          accessibilityLabel="Export to .CSV"
+          />
+      </ScrollView>
+    </View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+
+  contentContainer: {
+    paddingTop: 30,
+  },
+
+  logoContainer: {
+    alignItems: 'center',
+    height: 150,
+  },
+});
